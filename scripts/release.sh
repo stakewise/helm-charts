32839300ed0354d55910ada5c03cfb5446f4c1ff
Andrey Pronin <unxnn@users.noreply.github.com>
2021-11-18 16:51:46 +0300

    Debug CI

--- scripts/release.sh
+++ scripts/release.sh
@@ -19,20 +19,34 @@ helm gcs init ${GCS_BUCKET_NAME}
 echo "Adding gcs bucket repo ${GCS_BUCKET_NAME}"
 helm repo add private ${GCS_BUCKET_NAME}
 
+# add dependent helm repos
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add hashicorp https://helm.releases.hashicorp.com
+
 prev_rev=$(git rev-parse HEAD^)
 echo "Identifying changed charts since git rev ${prev_rev}"
 
 changed_charts=()
-readarray -t changed_charts <<< "$(git diff --find-renames --diff-filter=d --name-only "$prev_rev" -- charts | cut -d '/' -f 2 | uniq)"
+# readarray -t changed_charts <<< "$(git diff --find-renames --diff-filter=d --name-only "$prev_rev" -- charts | cut -d '/' -f 2 | uniq)"
 
-if [[ -n "${changed_charts[*]}" ]]; then
-    for chart in "${changed_charts[@]}"; do
-        echo "Packaging chart '$chart'..."
-        chart_file=$(helm package "charts/$chart" | awk '{print $NF}')
+# if [[ -n "${changed_charts[*]}" ]]; then
+#     for chart in "${changed_charts[@]}"; do
+#         echo "Packaging chart '$chart'..."
+#         helm dependency update charts/$chart
+#         chart_file=$(helm package "charts/$chart" | awk '{print $NF}')
 
-        echo "Pushing $chart_file..."
-        helm gcs push "$chart_file" private
-    done
-else
-    echo "No chart changes detected"
-fi
+#         echo "Pushing $chart_file..."
+#         helm gcs push "$chart_file" private
+#     done
+# else
+#     echo "No chart changes detected"
+# fi
+
+for chart in "charts/*"; do
+    echo "Packaging chart '$chart'..."
+    helm dependency update charts/$chart
+    chart_file=$(helm package "charts/$chart" | awk '{print $NF}')
+
+    echo "Pushing $chart_file..."
+    helm gcs push "$chart_file" private
+done
