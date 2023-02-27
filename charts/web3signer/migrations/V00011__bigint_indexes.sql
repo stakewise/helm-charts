6b4a0685661fea2940fae58678501834bb94dd54
antares-sw <23400824+antares-sw@users.noreply.github.com>
2023-02-27 11:56:01 +0400

    Fix migrations (#322)

--- charts/web3signer/migrations/V00011__bigint_indexes.sql
+++
@@ -1,7 +0,0 @@
-ALTER SEQUENCE signed_attestations_id_seq AS bigint;
-ALTER TABLE signed_attestations ALTER COLUMN id TYPE bigint;
-
-ALTER SEQUENCE signed_blocks_id_seq AS bigint;
-ALTER TABLE signed_blocks ALTER COLUMN id TYPE bigint;
-
-UPDATE database_version SET version = 11 WHERE id = 1;
