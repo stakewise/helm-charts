7024e1f8bc3fd88af01183218f05a4e2d5c6ce3f
Aivaras Ko <aivaras.ko@gmail.com>
2023-08-17 11:02:41 +0300

    fixes

--- charts/validators-eso/watcher.sh
+++ charts/validators-eso/watcher.sh
@@ -6,14 +6,14 @@ mkdir -p /tmp/data
 
 while true;
 do
-    poetry run keystore-cli
-    sync-validator-keys
-    --db-url $(ESO_DB_KEYSTORE_URL)
-    --output-dir /tmp/data
-    --index $1
+    poetry run keystore-cli \
+    sync-validator-keys \
+    --db-url "$ESO_DB_KEYSTORE_URL" \
+    --output-dir /tmp/data \
+    --index "$1" \
     --no-of-keys {{ .Values.validatorsNoOfKeys }};
     echo "a";
-    if ! diff /tmp/data/validator_definitions.yml /data/validator_definitions.yml; then kubectl delete pod $POD_NAME; fi;
+    if ! diff /tmp/data/validator_definitions.yml /data/validator_definitions.yml; then kubectl delete pod "$POD_NAME"; fi;
     sleep 60 &
     wait $!;
 done
